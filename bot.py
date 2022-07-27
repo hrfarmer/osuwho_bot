f38da4f1dddc309abb6be53a8f47b3f1750c5330
hrfarmer <58487401+hrfarmer@users.noreply.github.com>
2022-07-26 20:22:28 -0500

    queue system implemented

--- bot.py
+++ bot.py
@@ -155,14 +155,14 @@ class Bot(commands.Bot):
     @commands.command()
     async def die(self, ctx: commands.Context):
         if ctx.message.author.name == "hrfarmer_" or ctx.message.author.name == 'osuwhotest':
-            await ctx.send("DIESOFCRINGE *dies*")
+            await queue.put("DIESOFCRINGE *dies*")
             exit()
         else:
-            await ctx.send("PogO YOU ARE NOT WORTHY ENOUGH TO MURDER ME")
+            await queue.put("PogO YOU ARE NOT WORTHY ENOUGH TO MURDER ME")
 
     # @commands.command()
     # async def ac(self, ctx: commands.Context):
-    #     await ctx.send("!ac")
+    #     await queue.put("!ac")
 
     @commands.command()
     async def start(self, ctx: commands.Context):
@@ -171,7 +171,7 @@ class Bot(commands.Bot):
     @commands.command()
     async def reload_animes(self, ctx: commands.Context):
         get_anime.collect_animes()
-        await ctx.send("Anime list reloaded")
+        await queue.put("Anime list reloaded")
 
     @commands.command()
     async def search_anime_rank(self, ctx: commands.Context):
@@ -183,10 +183,10 @@ class Bot(commands.Bot):
             anime_lower = animes_lower.index(message_lower)
             anime_name = animes[anime_lower]
         except ValueError:
-            return await ctx.send("That anime is not found")
+            return await queue.put("That anime is not found")
 
         anime_lower += 1
-        await ctx.send(f"{anime_name} is at rank {anime_lower}")
+        await queue.put(f"{anime_name} is at rank {anime_lower}")
 
     @commands.command()
     async def random_animes(self, ctx: commands.Context):
@@ -194,16 +194,16 @@ class Bot(commands.Bot):
             message = int(ctx.message.content[15:])
             message += 1
         except ValueError:
-            return await ctx.send("Put a number after the command")
+            return await queue.put("Put a number after the command")
 
         if message > 6:
-            return await ctx.send("Limited to 5 at a time so you don't burn down chat Stare")
+            return await queue.put("Limited to 5 at a time so you don't burn down chat Stare")
 
         for i in range(1, message):
             r = random.randint(0, 1000)
             anime = animes[r]
             r += 1
-            await ctx.send(f"Anime: {anime} | Rank: {r}")
+            await queue.put(f"Anime: {anime} | Rank: {r}")
             await asyncio.sleep(1.5)
 
     @commands.command()
@@ -222,9 +222,9 @@ class Bot(commands.Bot):
             id = user_anime['data'][0]['node']['id']
             score = user_anime['data'][0]['list_status']['score']
         except:
-            return await ctx.send("User not found or something broke")
+            return await queue.put("User not found or something broke")
 
-        await ctx.send(f"{message}'s recent anime: {title} | Score: {score} 🌟 | Link: https://myanimelist.net/anime/{id}")
+        await queue.put(f"{message}'s recent anime: {title} | Score: {score} 🌟 | Link: https://myanimelist.net/anime/{id}")
 
     @commands.command()
     async def dink(self, ctx: commands.Context):
@@ -235,20 +235,20 @@ class Bot(commands.Bot):
     async def repeat(self, ctx: commands.Context):
         message = ctx.message.content
         message = message[8:]
-        await ctx.send(message)
+        await queue.put(message)
 
     @commands.command()
     async def roll(self, ctx: commands.Context):
         number = random.randint(1, 1000)
-        await ctx.send(f"@{ctx.message.author.name}, you rolled {number}")
+        await queue.put(f"@{ctx.message.author.name}, you rolled {number}")
 
     @commands.command()
     async def aiwebsite(self, ctx: commands.Context):
-        await ctx.send(f"@{ctx.message.author.name}, https://beta.openai.com/playground?mode=complete")
+        await queue.put(f"@{ctx.message.author.name}, https://beta.openai.com/playground?mode=complete")
 
     @commands.command()
     async def cytube(self, ctx: commands.Context):
-        await ctx.send(f"@{ctx.message.author.name}, https://cytu.be/r/enyoters")
+        await queue.put(f"@{ctx.message.author.name}, https://cytu.be/r/enyoters")
 
     @commands.command()
     async def cbot(self, ctx: commands.Context):
@@ -257,15 +257,15 @@ class Bot(commands.Bot):
         message = message[6:]
 
         cbot_message = await cbot_chat(message)
-        await ctx.send(f";cbot {cbot_message}")
+        await queue.put(f";cbot {cbot_message}")
 
     @commands.command()
     async def ai(self, ctx: commands.Context):
-        return await ctx.send("Ai is gone because I have no more openai credits LULW")
+        return await queue.put("Ai is gone because I have no more openai credits LULW")
         if ctx.message.author.name == "styx_e_clap":
-            return await ctx.send(f"@{ctx.message.author.name} fuck you you're banned FRICK")
+            return await queue.put(f"@{ctx.message.author.name} fuck you you're banned FRICK")
 
-        await ctx.send("ppHop Loading (other commands may or may not run after the ai has responded, resend if it doesn't)")
+        await queue.put("ppHop Loading (other commands may or may not run after the ai has responded, resend if it doesn't)")
         message = ctx.message.content
         message.join(filter(lambda x: x in printable, message))
         message = message[4:]
@@ -286,11 +286,11 @@ class Bot(commands.Bot):
             n = 450
             split_text = [text[i:i+n] for i in range(0, len(text), n)]
             for _text in split_text:
-                await ctx.send(f"@{ctx.message.author.name}, {_text}")
+                await queue.put(f"@{ctx.message.author.name}, {_text}")
                 await asyncio.sleep(1.5)
 
         else:
-            await ctx.send(f"@{ctx.message.author.name}, {text}")
+            await queue.put(f"@{ctx.message.author.name}, {text}")
 
     @commands.command()
     async def link(self, ctx: commands.Context):
@@ -304,21 +304,21 @@ class Bot(commands.Bot):
         try:
             self.yep = db.return_osu_username(self.twitch_username)
             if self.yep != None:
-                await ctx.send("NOPERS Tssk you can't link more than one account (do ?unlink to remove previous account)")
+                await queue.put("NOPERS Tssk you can't link more than one account (do ?unlink to remove previous account)")
         except:
             pass
 
         try:
             self.yep = await client.get_user(key="username", user=self.osu_username)
         except:
-            return await ctx.send("This is not a valid account")
+            return await queue.put("This is not a valid account")
 
         try:
             db.add_osu_username(self.twitch_username, self.osu_username)
         except:
-            return await ctx.send("Failed to link (ping hr man)")
+            return await queue.put("Failed to link (ping hr man)")
 
-        await ctx.send(f"Linked {self.twitch_username} to {self.osu_username}")
+        await queue.put(f"Linked {self.twitch_username} to {self.osu_username}")
 
     @commands.command()
     async def checkuser(self, ctx: commands.Context):
@@ -326,9 +326,9 @@ class Bot(commands.Bot):
         self.osu_username = db.return_osu_username(self.twitch_username)
 
         if self.osu_username == None:
-            return await ctx.send("You aren't linked to an osu account!")
+            return await queue.put("You aren't linked to an osu account!")
 
-        await ctx.send(f"@{self.twitch_username}, your linked osu! username is {self.osu_username[1]}")
+        await queue.put(f"@{self.twitch_username}, your linked osu! username is {self.osu_username[1]}")
 
     @commands.command()
     async def unlink(self, ctx: commands.Context):
@@ -336,10 +336,10 @@ class Bot(commands.Bot):
         try:
             self.osu_username = db.return_osu_username(self.twitch_username)
         except:
-            return await ctx.send("You aren't linked to an account")
+            return await queue.put("You aren't linked to an account")
 
         db.remove_osu_username(self.twitch_username)
-        await ctx.send(f"Unlinked {self.twitch_username} from {self.osu_username[1]}")
+        await queue.put(f"Unlinked {self.twitch_username} from {self.osu_username[1]}")
 
     @commands.command()
     async def rs(self, ctx: commands.Context):
@@ -353,12 +353,12 @@ class Bot(commands.Bot):
                 user = await client.get_user(key="username", user=username[1])
                 id = user.id
             except:
-                return await ctx.send("Please link your username with ?link")
+                return await queue.put("Please link your username with ?link")
         else:
             try:
                 user = await client.get_user(key="username", user=username)
             except:
-                return await ctx.send("Either you did not enter a username or the username entered is incorrect/does not exist.")
+                return await queue.put("Either you did not enter a username or the username entered is incorrect/does not exist.")
             id = user.id
 
         scores = await client.get_user_scores(
@@ -370,23 +370,23 @@ class Bot(commands.Bot):
                 if score.beatmap.status == 'graveyard' or score.beatmap.status == 'wip' or score.beatmap.status == 'pending':
                     s = f"@{ctx.message.author.name} {score.beatmapset.artist} - {score.beatmapset.title} [{score.beatmap.version}]: {score.score} | [{score.statistics.count_300}/{score.statistics.count_100}/{score.statistics.count_50}/{score.statistics.count_miss}] | {accuracy}% | Not ranked (can't get pp) | https://osu.ppy.sh/b/{score.beatmap.id}"
                     print(s)
-                    await ctx.send(s)
+                    await queue.put(s)
                 elif score.beatmap.status == 'loved':
                     s = f"@{ctx.message.author.name} {score.beatmapset.artist} - {score.beatmapset.title} [{score.beatmap.version}]: {score.score} | [{score.statistics.count_300}/{score.statistics.count_100}/{score.statistics.count_50}/{score.statistics.count_miss}] | {accuracy}% | Loved (can't get pp) | https://osu.ppy.sh/b/{score.beatmap.id}"
                     print(s)
-                    await ctx.send(s)
+                    await queue.put(s)
                 elif score.beatmap.status == 'qualified':
                     s = f"@{ctx.message.author.name} {score.beatmapset.artist} - {score.beatmapset.title} [{score.beatmap.version}]: {score.score} | [{score.statistics.count_300}/{score.statistics.count_100}/{score.statistics.count_50}/{score.statistics.count_miss}] | {accuracy}% | Qualified (can't get pp) | https://osu.ppy.sh/b/{score.beatmap.id}"
                     print(s)
-                    await ctx.send(s)
+                    await queue.put(s)
                 else:
                     s = f"@{ctx.message.author.name} {score.beatmapset.artist} - {score.beatmapset.title} [{score.beatmap.version}]: {score.score} | [{score.statistics.count_300}/{score.statistics.count_100}/{score.statistics.count_50}/{score.statistics.count_miss}] | {accuracy}% | {score.pp}pp | https://osu.ppy.sh/b/{score.beatmap.id}"
                     print(s)
-                    await ctx.send(s)
+                    await queue.put(s)
             else:
                 s = f"@{ctx.message.author.name} (FAILED) {score.beatmapset.artist} - {score.beatmapset.title} [{score.beatmap.version}]: {score.score} | [{score.statistics.count_300}/{score.statistics.count_100}/{score.statistics.count_50}/{score.statistics.count_miss}] | {accuracy}% | https://osu.ppy.sh/b/{score.beatmap.id}"
                 print(s)
-                await ctx.send(s)
+                await queue.put(s)
 
     # lastfm commands
 
@@ -400,21 +400,21 @@ class Bot(commands.Bot):
         self.lastfm_username = self.lastfm_username[13:]
 
         if self.lastfm_username == "":
-            return await ctx.send("Your username can't be blank PogO")
+            return await queue.put("Your username can't be blank PogO")
 
         try:
             self.yep = db.return_lastfm_username(self.twitch_username)
             if self.yep != None:
-                return await ctx.send("NOPERS Tssk you can't link more than one account (do ?lastfm_unlink to remove previous account)")
+                return await queue.put("NOPERS Tssk you can't link more than one account (do ?lastfm_unlink to remove previous account)")
         except:
             pass
 
         try:
             db.add_lastfm_username(self.twitch_username, self.lastfm_username)
         except:
-            return await ctx.send("Failed to link (ping hr man)")
+            return await queue.put("Failed to link (ping hr man)")
 
-        await ctx.send(f"Linked {self.twitch_username} to {self.lastfm_username}")
+        await queue.put(f"Linked {self.twitch_username} to {self.lastfm_username}")
 
     @commands.command()
     async def lastfm_checkuser(self, ctx: commands.Context):
@@ -422,9 +422,9 @@ class Bot(commands.Bot):
         self.lastfm_username = db.return_lastfm_username(self.twitch_username)
 
         if self.lastfm_username == None:
-            return await ctx.send("You aren't linked to a LastFM account!")
+            return await queue.put("You aren't linked to a LastFM account!")
 
-        await ctx.send(f"@{self.twitch_username}, your linked LastFM username is {self.lastfm_username[1]}")
+        await queue.put(f"@{self.twitch_username}, your linked LastFM username is {self.lastfm_username[1]}")
 
     @commands.command()
     async def lastfm_unlink(self, ctx: commands.Context):
@@ -433,10 +433,10 @@ class Bot(commands.Bot):
             self.lastfm_username = db.return_lastfm_username(
                 self.twitch_username)
         except:
-            return await ctx.send("You aren't linked to an account")
+            return await queue.put("You aren't linked to an account")
 
         db.remove_lastfm_username(self.twitch_username)
-        await ctx.send(f"Unlinked {self.twitch_username} from {self.lastfm_username[1]}")
+        await queue.put(f"Unlinked {self.twitch_username} from {self.lastfm_username[1]}")
 
     @commands.command()
     async def np(self, ctx: commands.Context):
@@ -448,7 +448,7 @@ class Bot(commands.Bot):
             try:
                 username = db.return_lastfm_username(ctx.message.author.name)
             except:
-                return await ctx.send(f"Please link a username with ?lastfm_link")
+                return await queue.put(f"Please link a username with ?lastfm_link")
 
         recent_song = lastfm.getRecentSong(username)
 
@@ -457,17 +457,17 @@ class Bot(commands.Bot):
             song_artist = recent_song['recenttracks']['track'][0]['artist']['name']
             song_url = recent_song['recenttracks']['track'][0]['url']
         except:
-            return await ctx.send("Either that username is invalid, or something broke LULW")
+            return await queue.put("Either that username is invalid, or something broke LULW")
 
-        await ctx.send(f"Now playing for {username[1]}: {song_artist} - {song_title} | {song_url}")
+        await queue.put(f"Now playing for {username[1]}: {song_artist} - {song_title} | {song_url}")
 
     @commands.command()
     async def lastfm_instructions(self, ctx: commands.Context):
-        await ctx.send(f"@{ctx.message.author.name}, https://pastebin.com/raw/ppReYSDm")
+        await queue.put(f"@{ctx.message.author.name}, https://pastebin.com/raw/ppReYSDm")
 
     @commands.command()
     async def lastfm_commands(self, ctx: commands.Context):
-        await ctx.send(f"@{ctx.message.author.name}, The commands for LastFM are ?np, ?lastfm_link, ?lastfm_unlink, ?lastfm_checkuser, and ?lastfm_instructions")
+        await queue.put(f"@{ctx.message.author.name}, The commands for LastFM are ?np, ?lastfm_link, ?lastfm_unlink, ?lastfm_checkuser, and ?lastfm_instructions")
 
     # chatstats commands
     @commands.command()
@@ -475,29 +475,34 @@ class Bot(commands.Bot):
         username = ctx.message.author.name.lower()
         messages = db.return_messages(username)
         if messages == None:
-            return await ctx.send(f"@{ctx.message.author.name} You are not on the chatter leaderboard pepePoint")
+            return await queue.put(f"@{ctx.message.author.name} You are not on the chatter leaderboard pepePoint")
 
-        await ctx.send(f"{messages[0]} is rank {messages[2]} with {messages[1]} messages sent in BTMC (data from https://stats.streamelements.com/c/btmc )")
+        await queue.put(f"{messages[0]} is rank {messages[2]} with {messages[1]} messages sent in BTMC (data from https://stats.streamelements.com/c/btmc )")
 
     @commands.command()
     async def refresh_stats(self, ctx: commands.Context):
         if ctx.message.author.name == 'hrfarmer_' or ctx.message.author.name == 'osuwhotest':
-            await ctx.send("ppCircle refreshing chatstats")
+            await queue.put("ppCircle refreshing chatstats")
             db.add_chatters()
-            await ctx.send("ppL done")
+            await queue.put("ppL done")
         else:
-            await ctx.send("PogO you don't have perms for this")
+            await queue.put("PogO you don't have perms for this")
 
     @commands.command()
     async def lb(self, ctx: commands.Context):
         if ctx.message.author.name == 'styx_e_clap':
-            return await ctx.send("FRICK im not letting you use this until theres a cooldown")
+            return await queue.put("FRICK im not letting you use this until theres a cooldown")
         top10 = db.return_top10()
         message = ""
         for person in top10:
             message = message + \
                 f"#{person[2]} {person[0]}: {person[1]} messages | "
-        await ctx.send(message)
+        await queue.put(message)
+
+    @commands.command()
+    async def spam(self, ctx: commands.Context):
+        for number in range(1, 11):
+            await queue.put(f"spam test {number}")
 
 
 bot = Bot()
@@ -510,8 +515,6 @@ async def send_message():
         m = await queue.get()
         await bot.connected_channels[0].send(m)
         await asyncio.sleep(1.5)
-
-        return
 
 asyncio.run_coroutine_threadsafe(send_message(), bot.loop)
 asyncio.run(bot.run())
